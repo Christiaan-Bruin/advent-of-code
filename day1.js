41b80a8a93b3ade7f3dd038e2bcf84363df734ca
Christiaan-Bruin <christiaan.bruin@bettyblocks.com>
2021-12-01 09:15:31 +0100

    day1

---
+++ day1.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const lines = fs
+  .readFileSync("day1.txt", { encoding: "utf-8" })
+  .split("\n")
+  .filter((x) => x)
+  .map((x) => parseInt(x));
+
+// Part 1
+let increased = 0;
+
+for (let i = 0; i < lines.length; i++) {
+  if (lines[i + 1] > lines[i]) {
+    increased++;
+  }
+}
+
+console.log(increased);
+
+// Part 2
+increased = 0;
+
+for (let i = 0; i < lines.length; i++) {
+  if (
+    lines[i] + lines[i + 1] + lines[i + 2] <
+    lines[i + 1] + lines[i + 2] + lines[i + 3]
+  ) {
+    increased++;
+  }
+}
+
+console.log(increased);
