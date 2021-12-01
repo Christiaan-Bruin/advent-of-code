c8cd1947b8839238f29b73ec8b206f4734bbbaf8
Christiaan-Bruin <christiaan.bruin@bettyblocks.com>
2021-12-01 16:44:38 +0100

    code cleanup

--- day1.js
+++ day1.js
@@ -6,27 +6,34 @@ const lines = fs
   .filter((x) => x)
   .map((x) => parseInt(x));
 
+const isBigger = (a, b) => {
+  return b > a;
+};
+
 // Part 1
-let increased = 0;
+const Part1 = (array) => {
+  let increased = 0;
 
-for (let i = 1; i < lines.length; i++) {
-  if (lines[i - 1] < lines[i]) {
-    increased++;
+  for (let i = 1; i < array.length; i++) {
+    if (isBigger(array[i - 1], array[i])) increased++;
   }
-}
 
-console.log(increased);
+  return increased;
+};
 
 // Part 2
-increased = 0;
+const Part2 = (array) => {
+  let increased = 0;
 
-for (let i = 3; i < lines.length; i++) {
-  if (
-    lines[i] + lines[i - 1] + lines[i - 2] >
-    lines[i - 1] + lines[i - 2] + lines[i - 3]
-  ) {
-    increased++;
+  for (let i = 3; i < array.length; i++) {
+    let prevWindow = array[i - 1] + array[i - 2] + array[i - 3];
+    let nextWindow = array[i] + array[i - 1] + array[i - 2];
+
+    if (isBigger(prevWindow, nextWindow)) increased++;
   }
-}
 
-console.log(increased);
+  return increased;
+};
+
+console.log("Part 1: " + Part1(lines));
+console.log("Part 2: " + Part2(lines));
